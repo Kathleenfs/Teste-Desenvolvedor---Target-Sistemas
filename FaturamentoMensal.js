9195a351dea70cc6009265256591ad7d9e52ed46
kath <kathynik07@gmail.com>
2022-06-20 20:33:49 -0300

    Atividade 3

---
+++ FaturamentoMensal.js
@@ -0,0 +1,155 @@
+const mes = [{
+    "dia": 1,
+    "valor": 22174.1664
+},
+{
+    "dia": 2,
+    "valor": 24537.6698
+},
+{
+    "dia": 3,
+    "valor": 26139.6134
+},
+{
+    "dia": 4,
+    "valor": 0.0
+},
+{
+    "dia": 5,
+    "valor": 0.0
+},
+{
+    "dia": 6,
+    "valor": 26742.6612
+},
+{
+    "dia": 7,
+    "valor": 0.0
+},
+{
+    "dia": 8,
+    "valor": 42889.2258
+},
+{
+    "dia": 9,
+    "valor": 46251.174
+},
+{
+    "dia": 10,
+    "valor": 11191.4722
+},
+{
+    "dia": 11,
+    "valor": 0.0
+},
+{
+    "dia": 12,
+    "valor": 0.0
+},
+{
+    "dia": 13,
+    "valor": 3847.4823
+},
+{
+    "dia": 14,
+    "valor": 373.7838
+},
+{
+    "dia": 15,
+    "valor": 2659.7563
+},
+{
+    "dia": 16,
+    "valor": 48924.2448
+},
+{
+    "dia": 17,
+    "valor": 18419.2614
+},
+{
+    "dia": 18,
+    "valor": 0.0
+},
+{
+    "dia": 19,
+    "valor": 0.0
+},
+{
+    "dia": 20,
+    "valor": 35240.1826
+},
+{
+    "dia": 21,
+    "valor": 43829.1667
+},
+{
+    "dia": 22,
+    "valor": 18235.6852
+},
+{
+    "dia": 23,
+    "valor": 4355.0662
+},
+{
+    "dia": 24,
+    "valor": 13327.1025
+},
+{
+    "dia": 25,
+    "valor": 0.0
+},
+{
+    "dia": 26,
+    "valor": 0.0
+},
+{
+    "dia": 27,
+    "valor": 25681.8318
+},
+{
+    "dia": 28,
+    "valor": 1718.1221
+},
+{
+    "dia": 29,
+    "valor": 13220.495
+},
+{
+    "dia": 30,
+    "valor": 8414.61
+}]
+
+
+let maior = 0
+let menor = 0
+let soma = 0
+let somaDia = 0
+let contaDias = 0
+
+for(i = 0; i < mes.length; i++){
+    const conversao = parseFloat(mes[i].valor.toFixed(3))
+    if (mes[i].valor != 0){
+    contaDias = contaDias + 1
+    }
+    
+    soma += mes[i].valor
+
+    if(mes[i].valor > maior){
+        maior = mes[i].valor
+    }else{
+        menor = mes[i].valor
+    }
+
+}
+
+const media = soma/contaDias
+
+
+for(i = 0; i < mes.length; i++){
+    if(mes[i].valor > media){
+        somaDia = somaDia + 1
+    }
+}
+console.log('Quantidade de dias que tiveram o valor do faturamento diario maior que a media mensal foram de '+ somaDia + " dias")
+console.log('O menor valor foi: ' + menor.toFixed(2) + 'O maior valor foi: '+ maior.toFixed(2))
+
